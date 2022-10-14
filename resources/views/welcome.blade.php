f3fb8df512bbc16a5e07f841ac175e368b118a75
wied artha pratama <wiedarthapratama@gmail.com>
2022-10-14 14:44:13 +0700

    auth with jwt

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" >
+   <head>
+      <meta charset="UTF-8">
+      <title>API by WAP</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style>@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@600&family=Economica&display=swap'); body{/* or any other parent element */ position: relative; /* usually the parent, in this case the body element, has position relative so that the absolute positioned child is positioned relative to it */ height: 100vh; /* 100% of the viewport height */ margin: 0; /* recommended */ background-color: #2f3640; font-family: 'Caveat', cursive;}h1{position: absolute; /* taken out of the normal flow of the document */ top: 40%; /* moved down by 50% of the screen height */ transform: translateY(-50%); /* moved back up (Y axis) by half of its height to achieve the perfect center */ width: 100%; /* needs to be defined to keep the default block behavior */ text-align: center; margin: 0; /* again, for perfect center */ color: white; font-size: 8rem;}p a{position: absolute; /* taken out of the normal flow of the document */ top: 59%; /* moved down by 50% of the screen height */ transform: translateY(-50%); /* moved back up (Y axis) by half of its height to achieve the perfect center */ width: 100%; /* needs to be defined to keep the default block behavior */ text-align: center; margin: 0; /* again, for perfect center */ color: white; font-family: 'Economica', sans-serif;}</style>
+   </head>
+   <body>
+      <div>
+         <h1>Api Skripsi <br> Wied Artha Pratama</h1>
+      </div>
+   </body>
+</html>
